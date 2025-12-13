ebcaee18e813c5739ae5da681b7561060ae781e9
George White <me@galexite.uk>
2025-12-13 20:19:01 +0000

    ruff format

--- txc2gtfs/trips.py
+++ txc2gtfs/trips.py
@@ -9,10 +9,11 @@ if TYPE_CHECKING:
 def get_trips(gtfs_info: pd.DataFrame) -> pd.DataFrame:
     """Extract trips attributes from GTFS info DataFrame"""
     # Extract trips from GTFS info
-    return cast(pd.DataFrame,
+    return cast(
+        pd.DataFrame,
         gtfs_info[
             ["route_id", "service_id", "trip_id", "trip_headsign", "direction_id"]
         ]
         .drop_duplicates()
-        .reset_index(drop=True)
+        .reset_index(drop=True),
     )
